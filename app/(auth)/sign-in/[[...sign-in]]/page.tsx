fa2385a6045458fd2629bb808228421a418d3dac
Timur Isachenko <t.isachenko@quality-wolves.com>
2024-09-09 20:03:34 +0200

    init

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,33 @@
+import { ClerkLoaded, ClerkLoading, SignIn } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+        <div className='h-full lg:flex flex-col justify-center items-center px-4'>
+            <div className='text-center space-y-4 pt-16'>
+                <h1 className='font-bold text-3xl text-[#2E2A47]'>
+                    Welcome back
+                    </h1>
+                    <p className='text-base text-[#7E8CA0]'>
+                        Log in or Create account to get to your dashboard!
+                    </p>
+            </div>
+            <div className='flex justify-center items-center mt-8'>
+              <ClerkLoaded>
+                <SignIn path='/sign-in'/>
+              </ClerkLoaded>
+              <ClerkLoading>
+                <div className='flex justify-center items-center mt-8'>
+                  <Loader2 className='w-6 h-6 animate-spin text-muted-foreground' />
+                </div>
+              </ClerkLoading>
+            </div>
+        </div>
+        <div className='h-full bg-blue-600 hidden lg:flex justify-center items-center'>
+            <Image src='/logo.svg' alt='Logo' width={300} height={300} />
+        </div>
+    </div>
+  )
+}
---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+import accounts from "./accounts";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+const routes = app.route("/accounts", accounts);
+
+export const GET = handle(routes);
+export const POST = handle(routes);
+
+export type ApiRouter = typeof routes;
